cc9f5384728a280ca3e0df7fdc5c21d4a9b28857
Jakseer <2292056398@qq.com>
2018-02-08 16:30:13 +0800

    【添加】编写了 链表类及相应操作

---
+++ 5-反转链表.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct Node{
+	int value;
+	Node *next;
+};
+
+class LinkList{
+private:
+	Node *p = NULL;
+public:
+	void addNode(int val);
+	bool delNode(int val);
+	void travel();
+	void reverse();
+};
+
+void LinkList::addNode(int val){
+	Node *node = new Node();
+	if(node == NULL)
+		return;
+	node->value = val;
+	node->next = NULL;
+	if(this->p == NULL){
+		this->p = node;
+	}else{
+		Node *x = this->p;
+        while(x->next != NULL){
+            x = x->next;
+        }
+        x->next = node;
+	}
+}
+
+bool LinkList::delNode(int val){
+	if(this->p == NULL)
+		return false;
+
+	Node *x = this->p;
+	//如果第一个节点是要删除的节点
+	if(x->value == val){
+		this->p = x->next;
+		delete x;
+		return true;
+	}
+	while(x->next != NULL){
+        if(x->next->value == val){
+			Node *tmp = x->next;
+			x->next = x->next->next;
+			delete tmp;
+			return true;
+        }
+        x = x->next;
+	}
+	return false;
+}
+
+void LinkList::travel(){
+	if(this->p == NULL)
+		return;
+	Node *x = this->p;
+	cout<<"遍历链表：";
+    while(x->next != NULL){
+        cout<<x->value<<" ";
+        x = x->next;
+    }
+    //打印最后一个节点的值
+    cout<<x->value<<endl;
+
+}
+
+void LinkList::reverse(){
+}
+
+
+void testLinkList(){
+	LinkList a;
+	if(a.delNode(6)){
+		cout<<"删除节点成功"<<endl;
+	}else{
+		cout<<"删除节点失败"<<endl;
+	}
+
+	a.addNode(1);
+	a.addNode(2);
+	a.addNode(3);
+	a.addNode(4);
+	a.addNode(5);
+	a.travel();
+	if(a.delNode(1)){
+		cout<<"删除节点1成功"<<endl;
+	}else{
+		cout<<"删除节点1失败"<<endl;
+	}
+	if(a.delNode(5)){
+		cout<<"删除节点5成功"<<endl;
+	}else{
+		cout<<"删除节点5失败"<<endl;
+	}
+	if(a.delNode(50)){
+		cout<<"删除节点50成功"<<endl;
+	}else{
+		cout<<"删除节点50失败"<<endl;
+	}
+	a.travel();
+
+}
+
+
+
+int main(){
+	testLinkList();
+	return 0;
+}
